8cdc761246bbe12372bf6b38e1e6b7f58463c060
Júlia Rodrigues dos Santos <julia.santos67@portalsesisp.org.br>
2023-05-16 15:00:13 -0300

    logica 16052023

--- login.js
+++ login.js
@@ -1,17 +1,21 @@
 const mensagem = document.querySelector(".mensagem");
 const email = document.getElementById("email");
 const senha = document.getElementById("senha");
-const formulario = document.getElementById ("formulario");
+const formulario = document.getElementById("formulario");
 
-formulario.onsubmit = (evento) => {
-    let dados = JSON.parse(localStorage.getItem("base"));
-
+formulario.onsubmit = (evento) =>{
+    let dados = JSON.parse(localStorage.getItem("dados"));
     dados.forEach((elemento) => {
-        if (elemento.email == email.value && elemento.senha == senha.value ) {
+        if (elemento.email === email.value && elemento.senha == senha.value){
             evento.preventDefault();
+            mensagem.innerHTML="Acesso permitido";
+            setTimeout(() => {
+                window.location.assign("catalogo.html")
+            }, 2000);
             return true;
-        } else {
-            mensagem.innerHTML="Senha ou E-mail incorreto"
+        }else{
+            evento.preventDefault();
+            mensagem.innerHTML="senha ou e-mail incorreto"
         }
     });
 }
